a96f3bd7ac0befeb917c0e200f479d5f3ffe9371
Klaus Tegtmeier <ktpunktneu@gmail.com>
2026-07-05 22:31:20 +0200

    PC-Sync entfernt, Hilfe-Button mit Kurzanleitung im Header ergaenzt

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vokabelit-v18';
+const CACHE = 'vokabelit-v19';
 const ASSETS = ['./', './index.html', './manifest.json', './static/icon-192.png', './static/icon-512.png'];
 
 self.addEventListener('install', e => {
--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vokabelit-v18';
+const CACHE = 'vokabelit-v19';
 const ASSETS = ['/', '/static/icon-192.png', '/static/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
