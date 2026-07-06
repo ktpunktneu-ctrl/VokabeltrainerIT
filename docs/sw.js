932146bb575958f268688828bb82b997692d24c6
Klaus Tegtmeier <ktpunktneu@gmail.com>
2026-07-06 18:32:09 +0200

    Master-Code fuer Eigennutzung ergaenzt (umgeht Gumroad-Pruefung)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vokabelit-v22';
+const CACHE = 'vokabelit-v23';
 const ASSETS = ['./', './index.html', './manifest.json', './static/icon-192.png', './static/icon-512.png'];
 
 self.addEventListener('install', e => {
--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vokabelit-v22';
+const CACHE = 'vokabelit-v23';
 const ASSETS = ['/', '/static/icon-192.png', '/static/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
