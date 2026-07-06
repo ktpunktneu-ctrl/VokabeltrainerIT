aa273055d37445fe846113b5e1f56e5aef92ada3
Klaus Tegtmeier <ktpunktneu@gmail.com>
2026-07-06 08:20:22 +0200

    Echtes Auto-Update: aktive Update-Pruefung + Auto-Reload bei neuer Service-Worker-Version

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vokabelit-v20';
+const CACHE = 'vokabelit-v21';
 const ASSETS = ['/', '/static/icon-192.png', '/static/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
