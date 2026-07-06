b0b9a9b9aaba9d3d14aaa5f2ece4927c0bc4100f
Klaus Tegtmeier <ktpunktneu@gmail.com>
2026-07-06 11:03:21 +0200

    Feature-Gating: Konjugation hinter Freischaltung, 14-Tage-Testphase, Gumroad-Lizenzpruefung, Bestandsnutzer automatisch freigeschaltet

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vokabelit-v21';
+const CACHE = 'vokabelit-v22';
 const ASSETS = ['/', '/static/icon-192.png', '/static/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
